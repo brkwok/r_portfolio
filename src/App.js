fee649df2ae5fcb7261040435b5b8d2da3e80de9
Brian Kwok <brkwok@gmail.com>
2021-10-09 16:33:02 -0400

    Projects and skills

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import Home from "./routes/Home";
-import Error from "./routes/Error";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <Home />
+      <Projects />
+      <Skills />
     </main>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,12 @@ export default function Home() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Brian.
-            <br className="hidden lg:inline-block" />I love to build amazing
-            apps.
+            <br className="hidden lg:inline-block" />I love to build apps and solve problems.
           </h1>
           <p className="mb-8 leading-relaxed">
-            Software Engineer based in NYC. <br />
-            Experienced with JavaScript, ReactJS, Ruby, Rails, PostgreSQL, and More.
+            Software Engineer based in NYC. <br className="hidden lg:inline-block"/>
+            Experienced with JavaScript, ReactJS, Ruby, Rails, PostgreSQL, and
+            More.
           </p>
           <div className="flex justify-center">
             <a
