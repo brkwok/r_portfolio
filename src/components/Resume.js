43c74c397a5eeff1587e61816d8fcf930922d983
Brian Kwok <brkwok@gmail.com>
2023-09-14 03:26:09 -0400

    Remove styles from resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,14 +4,6 @@ import React from "react";
 export default function Resume() {
 	return (
 		<section id="resume" className="text-gray-400 bg-gray-900 body-font">
-			<div className="container px-5 py-10 mx-auto text-center lg:px-40">
-				<div className="flex flex-col w-full">
-					<DocumentIcon className="mx-auto inline-block w-10 mb-4" />
-					<h1 className="sm:text-4xl text-3xl font-medium title-font text-white">
-						My Résumé
-					</h1>
-				</div>
-			</div>
 			<div className="flex justify-center">
 				<a
 					href="Resume.pdf"
